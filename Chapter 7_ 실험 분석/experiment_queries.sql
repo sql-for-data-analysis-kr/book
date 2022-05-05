987b2d4acda35b37ee5d5121476e50894efc453e
Sangkeun Park <pskalyber@gmail.com>
2022-05-06 00:38:38 +0900

    7장 실험 분석 쿼리 검토 및 한글 주석 추가

--- Chapter 7_ 실험 분석/experiment_queries.sql
+++ Chapter 7_ 실험 분석/experiment_queries.sql
@@ -1,5 +1,21 @@
------- Types of experiments
--- Experiments with binary outcomes
+-- 7장 실험 분석
+
+-- 7.3 실험 타입
+
+-- 7.3.1 이진형 결과의 실험: 카이제곱 검정
+
+
+SELECT a.variant
+,count(case when b.user_id is not null then a.user_id end) as completed
+,count(case when b.user_id is null then a.user_id end) as not_completed
+FROM exp_assignment a
+LEFT JOIN game_actions b on a.user_id = b.user_id
+ and b.action = 'onboarding complete'
+WHERE a.exp_name = 'Onboarding'
+GROUP BY 1
+;
+
+
 SELECT a.variant
 ,count(a.user_id) as total_cohorted
 ,count(b.user_id) as completions
@@ -11,23 +27,9 @@ WHERE a.exp_name = 'Onboarding'
 GROUP BY 1
 ;
 
--- Experiments with continuous outcomes
-SELECT variant
-,count(user_id) as total_cohorted
-,avg(amount) as mean_amount
-,stddev(amount) as stddev_amount
-FROM
-(
-        SELECT a.variant
-        ,a.user_id
-        ,sum(coalesce(b.amount,0)) as amount
-        FROM exp_assignment a
-        LEFT JOIN game_purchases b on a.user_id = b.user_id
-        WHERE a.exp_name = 'Onboarding'
-        GROUP BY 1,2
-) a
-GROUP BY 1
-;
+
+-- 7.3.2 연속형 결과의 실험: t-검정
+
 
 SELECT variant
 ,count(user_id) as total_cohorted
@@ -35,21 +37,44 @@ SELECT variant
 ,stddev(amount) as stddev_amount
 FROM
 (
-        SELECT a.variant
-        ,a.user_id
-        ,sum(coalesce(b.amount,0)) as amount
-        FROM exp_assignment a
-        LEFT JOIN game_purchases b on a.user_id = b.user_id
-        JOIN game_actions c on a.user_id = c.user_id
-        and c.action = 'onboarding complete'
-        WHERE a.exp_name = 'Onboarding'
-        GROUP BY 1,2
+    SELECT a.variant
+    ,a.user_id
+    ,sum(coalesce(b.amount,0)) as amount
+    FROM exp_assignment a
+    LEFT JOIN game_purchases b on a.user_id = b.user_id
+    WHERE a.exp_name = 'Onboarding'
+    GROUP BY 1,2
 ) a
 GROUP BY 1
 ;
 
------- Challenges with experiments
--- Outliers
+
+SELECT variant
+,count(user_id) as total_cohorted
+,avg(amount) as mean_amount
+,stddev(amount) as stddev_amount
+FROM
+(
+    SELECT a.variant
+    ,a.user_id
+    ,sum(coalesce(b.amount,0)) as amount
+    FROM exp_assignment a
+    LEFT JOIN game_purchases b on a.user_id = b.user_id
+    JOIN game_actions c on a.user_id = c.user_id
+    and c.action = 'onboarding complete'
+    WHERE a.exp_name = 'Onboarding'
+    GROUP BY 1,2
+) a
+GROUP BY 1
+;
+
+
+-- 7.4 실험의 어려움과 실험시 발생할 수 있는 문제 해결 방법
+
+
+-- 7.4.2 아웃라이어
+
+
 SELECT a.variant
 ,count(distinct a.user_id) as total_cohorted
 ,count(distinct b.user_id) as purchasers
@@ -62,27 +87,34 @@ WHERE a.exp_name = 'Onboarding'
 GROUP BY 1
 ;
 
--- Time boxing
+
+-- 7.4.3 타임박싱
+
+
 SELECT variant
 ,count(user_id) as total_cohorted
 ,avg(amount) as mean_amount
 ,stddev(amount) as stddev_amount
 FROM
 (
-        SELECT a.variant
-        ,a.user_id
-        ,sum(coalesce(b.amount,0)) as amount
-        FROM exp_assignment a
-        LEFT JOIN game_purchases b on a.user_id = b.user_id 
-        and b.purch_date <= a.exp_date + interval '7 days'
-        WHERE a.exp_name = 'Onboarding'
-        GROUP BY 1,2
+    SELECT a.variant
+    ,a.user_id
+    ,sum(coalesce(b.amount,0)) as amount
+    FROM exp_assignment a
+    LEFT JOIN game_purchases b on a.user_id = b.user_id 
+    and b.purch_date <= a.exp_date + interval '7 days'
+    WHERE a.exp_name = 'Onboarding'
+    GROUP BY 1,2
 ) a
 GROUP BY 1
 ;
 
------- When controlled experiments aren't possible
--- Pre-/post- analysis
+
+-- 7.5 대조 실험이 불가능한 경우: 대체 분석 방법
+
+
+-- 7.5.1 사전/사후 분석
+
 
 SELECT 
 case when a.created between '2020-01-13' and '2020-01-26' then 'pre'
@@ -97,9 +129,13 @@ LEFT JOIN game_actions b on a.user_id = b.user_id
 and b.action = 'email_optin'
 WHERE a.created between '2020-01-13' and '2020-02-09'
 GROUP BY 1
+ORDER BY 1 desc
 ;
 
--- Natural experiment analysis
+
+-- 7.5.2. 자연 실험 분석
+
+
 SELECT a.country
 ,count(distinct a.user_id) as total_cohorted
 ,count(distinct b.user_id) as purchasers
