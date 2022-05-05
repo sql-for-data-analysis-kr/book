db598f83c23fac6129bb838bdc1a3471d6dc8843
Sangkeun Park <pskalyber@gmail.com>
2022-05-06 00:55:01 +0900

    8장 쿼리 검토 및 한글 주석 추가

--- Chapter 8_ 복잡한 데이터셋 생성/complex_dataset_queries.sql
+++ Chapter 8_ 복잡한 데이터셋 생성/complex_dataset_queries.sql
@@ -1,4 +1,8 @@
------ Code organization 
+-- 8장 복잡한 데이터셋 생성
+
+-- 8.2 코드 구성 
+
+-- 8.2.2 대소문자, 들여쓰기, 괄호, 등의 포맷팅 요령
 
 SELECT type, mag
 ,case when place like '%CA%' then 'California'
@@ -12,7 +16,12 @@ and mag between 0 and 1
 GROUP BY 1,2,3
 ;
 
------ Organizing computations
+
+-- 8.3 쿼리 실행 순서 구성
+
+
+-- 8.3.1 SQL 절의 실행 순서
+
 
 SELECT state
 ,count(*) as terms
@@ -22,12 +31,14 @@ HAVING count(*) >= 1000
 ORDER BY 2 desc
 ;
 
+
 SELECT state
 ,count(*) as terms
 ,avg(count(*)) over () as avg_terms
 FROM legislators_terms
 GROUP BY 1
 ;
+
 
 SELECT state
 ,count(*) as terms
@@ -36,47 +47,56 @@ FROM legislators_terms
 GROUP BY 1
 ;
 
-SELECT date_part('year',c.first_term) as first_year
-,a.party
-,count(a.id_bioguide) as legislators
-FROM
-(
-        SELECT distinct id_bioguide, party
-        FROM legislators_terms
-        WHERE term_end > '2020-06-01'
-) a,
-LATERAL
-(
-        SELECT b.id_bioguide
-        ,min(term_start) as first_term
-        FROM legislators_terms b
-        WHERE b.id_bioguide = a.id_bioguide
-        and b.party <> a.party
-        GROUP BY 1
-) c
-GROUP BY 1,2
-;
+
+-- 8.3.2 서브쿼리
+
 
 SELECT date_part('year',c.first_term) as first_year
 ,a.party
 ,count(a.id_bioguide) as legislators
 FROM
 (
-        SELECT distinct id_bioguide, party
-        FROM legislators_terms
-        WHERE term_end > '2020-06-01'
+    SELECT distinct id_bioguide, party
+    FROM legislators_terms
+    WHERE term_end > '2020-06-01'
+) a,
+LATERAL
+(
+    SELECT b.id_bioguide
+    ,min(term_start) as first_term
+    FROM legislators_terms b
+    WHERE b.id_bioguide = a.id_bioguide
+    and b.party <> a.party
+    GROUP BY 1
+) c
+GROUP BY 1,2
+;
+
+
+SELECT date_part('year',c.first_term) as first_year
+,a.party
+,count(a.id_bioguide) as legislators
+FROM
+(
+    SELECT distinct id_bioguide, party
+    FROM legislators_terms
+    WHERE term_end > '2020-06-01'
 ) a
 JOIN
 (
-        SELECT id_bioguide, party
-        ,min(term_start) as first_term
-        FROM legislators_terms
-        GROUP BY 1,2
+    SELECT id_bioguide, party
+    ,min(term_start) as first_term
+    FROM legislators_terms
+    GROUP BY 1,2
 ) c on c.id_bioguide = a.id_bioguide and c.party <> a.party
 GROUP BY 1,2
 ;
 
 
+-- 8.3.3 임시 테이블
+
+
+DROP table if exists temp_states; -- 기존 테이블이 있다면 삭제
 CREATE temporary table temp_states
 (
 state varchar primary key
@@ -88,11 +108,17 @@ SELECT distinct state
 FROM legislators_terms
 ;
 
+
+DROP table if exists temp_states; -- 기존 테이블이 있다면 삭제
 CREATE temporary table temp_states
 as
 SELECT distinct state
 FROM legislators_terms
 ;    
+
+
+-- 8.3.4 공통 테이블 표현식
+
 
 WITH first_term as
 (
@@ -109,6 +135,10 @@ GROUP BY 1
 ;
 
 
+-- 8.3.5 GROUPING SETS
+
+
+(
 SELECT platform
 ,null as genre
 ,null as publisher
@@ -129,13 +159,18 @@ SELECT null as platform
 ,sum(global_sales) as global_sales
 FROM videogame_sales
 GROUP BY 1,2,3
+)
+ORDER BY 1,2,3
 ;
+
 
 SELECT platform, genre, publisher
 ,sum(global_sales) as global_sales
 FROM videogame_sales
 GROUP BY grouping sets (platform, genre, publisher)
+ORDER BY 1,2,3
 ;
+
 
 SELECT coalesce(platform,'All') as platform
 ,coalesce(genre,'All') as genre
@@ -146,6 +181,7 @@ GROUP BY grouping sets ((), platform, genre, publisher)
 ORDER BY 1,2,3
 ;
 
+
 SELECT coalesce(platform,'All') as platform
 ,coalesce(genre,'All') as genre
 ,coalesce(publisher,'All') as publisher
@@ -155,10 +191,12 @@ GROUP BY cube (platform, genre, publisher)
 ORDER BY 1,2,3
 ;
 
------ Managing data set size and privacy
-SELECT 123456 % 100;
 
-SELECT mod(123456,100);
+-- 8.4 데이터셋 크기 및 프라이버시 관리
+
+
+-- 8.4.2 차원 축소
+
 
 SELECT case when state in ('CA','TX','FL','NY','PA') then state 
             else 'Other' end as state_group
@@ -168,21 +206,23 @@ GROUP BY 1
 ORDER BY 2 desc
 ;
 
+
 SELECT case when b.rank <= 5 then a.state 
             else 'Other' end as state_group
 ,count(distinct id_bioguide) as legislators            
 FROM legislators_terms a 
 JOIN
 (
-        SELECT state
-        ,count(distinct id_bioguide)
-        ,rank() over (order by count(distinct id_bioguide) desc) 
-        FROM legislators_terms
-        GROUP BY 1
+    SELECT state
+    ,count(distinct id_bioguide)
+    ,rank() over (order by count(distinct id_bioguide) desc) 
+    FROM legislators_terms
+    GROUP BY 1
 ) b on a.state = b.state
 GROUP BY 1
 ORDER BY 2 desc
 ;
+
 
 SELECT case when terms >= 2 then true else false end as two_terms_flag
 ,count(*) as legislators
@@ -196,6 +236,7 @@ FROM
 GROUP BY 1
 ;
 
+
 SELECT 
 case when terms >= 10 then '10+'
      when terms >= 2 then '2 - 9'
@@ -203,18 +244,13 @@ case when terms >= 10 then '10+'
 ,count(*) as legislators
 FROM
 (
-        SELECT id_bioguide
-        ,count(term_id) as terms
-        FROM legislators_terms
-        GROUP BY 1
+    SELECT id_bioguide
+    ,count(term_id) as terms
+    FROM legislators_terms
+    GROUP BY 1
 ) a
 GROUP BY 1
 ;
-
-SELECT md5('my info');
-
-
-
 
 
 
